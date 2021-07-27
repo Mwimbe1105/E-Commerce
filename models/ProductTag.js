8e2c2d6bcef78a7d31af213f8ef389e1c31b4fa3
Mwimbe1105 <gyver1105@icloud.com>
2021-07-26 20:11:30 -0600

    models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true,
+    },
+
+    productID: {
+      type:DataTypes.INTEGER,
+      references:{
+        Key:"id",
+        model:"product",
+      },
+
+    tagID: {
+      type:DataTypes.INTEGER,
+      references:{
+        key: "id",
+        model:"tag",
+      }
+    }
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "categoryID"
+});
 
 // Categories have many Products
-
+Category.hasMany(product,{
+  foreignKey: "categoryID"
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(product,{
+  through:ProductTag
+});
 
 module.exports = {
   Product,
